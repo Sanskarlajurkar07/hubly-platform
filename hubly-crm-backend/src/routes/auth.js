962d4eac4cc2359a11539539681d821852990214
Sanskar Lajurkar <164156929+Sanskarlajurkar07@users.noreply.github.com>
2025-12-12 00:50:51 +0530

    Add verifyToken route to auth router

--- hubly-crm-backend/src/routes/auth.js
+++ hubly-crm-backend/src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerAdmin, loginUser, changePassword } = require('../controllers/authController');
+const { registerAdmin, loginUser, changePassword, verifyToken } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/signup', registerAdmin);
 router.post('/login', loginUser);
 router.put('/password', protect, changePassword);
+router.get('/verify', protect, verifyToken);
 
 module.exports = router;
